0a767948ec1c62592ff7894550d0487ce26a6ebb
silence <yangkemuyi@sina.com>
2019-11-28 15:51:20 +0800

    first commit

---
+++ LTGameSDKGuest/src/main/java/com/gentop/ltsdk/guest/GuestManager.java
@@ -0,0 +1,48 @@
+package com.gentop.ltsdk.guest;
+
+
+import android.content.Context;
+import android.text.TextUtils;
+
+import com.gentop.ltsdk.common.impl.OnLoginSuccessListener;
+import com.gentop.ltsdk.common.manager.LoginResultManager;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+public class GuestManager {
+
+    /**
+     * 游客登录
+     */
+    public static void guestLogin(Context context, String url, String LTAppID, String LTAppKey,
+                                  String adID, String packageID, OnLoginSuccessListener mListener) {
+        Map<String, Object> map = new WeakHashMap<>();
+        if (!TextUtils.isEmpty(adID)) {
+            map.put("platform", 2);
+            map.put("adid", adID);
+            map.put("gps_adid", adID);
+            map.put("platform_id", packageID);
+        }
+        LoginResultManager.guestLogin(context, url, LTAppID, LTAppKey, map, mListener);
+    }
+
+    /**
+     * 绑定账户
+     */
+    public static void bindAccount(Context context, String url, String LTAppID, String LTAppKey,
+                                   String adID, String packageID,String tag,String token,
+                                   OnLoginSuccessListener mListener) {
+        Map<String, Object> map = new WeakHashMap<>();
+        if (!TextUtils.isEmpty(adID)) {
+            map.put("access_token", token);
+            map.put("platform", 2);
+            map.put("adid", adID);
+            map.put("gps_adid", adID);
+            map.put("platform_id", packageID);
+            map.put("type", tag);
+        }
+        LoginResultManager.bingAccount(context, url, LTAppID, LTAppKey, map, mListener);
+    }
+
+}
