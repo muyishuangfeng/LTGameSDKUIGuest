ab7473ecf3e830a94a3c69ca193b2aa1e6163083
silence <yangkemuyi@sina.com>
2019-11-29 14:16:08 +0800

    更新

--- LTGameSDKGuest/src/main/java/com/gentop/ltsdk/guest/GuestManager.java
+++ LTGameSDKGuest/src/main/java/com/gentop/ltsdk/guest/GuestManager.java
@@ -15,7 +15,7 @@ public class GuestManager {
     /**
      * 游客登录
      */
-    public static void guestLogin(Context context, String url, String LTAppID, String LTAppKey,
+    public static void guestLogin(Context context, boolean isServerTest, String LTAppID, String LTAppKey,
                                   String adID, String packageID, OnLoginSuccessListener mListener) {
         Map<String, Object> map = new WeakHashMap<>();
         if (!TextUtils.isEmpty(adID)) {
@@ -24,13 +24,13 @@ public class GuestManager {
             map.put("gps_adid", adID);
             map.put("platform_id", packageID);
         }
-        LoginResultManager.guestLogin(context, url, LTAppID, LTAppKey, map, mListener);
+        LoginResultManager.guestLogin(context, isServerTest, LTAppID, LTAppKey, map, mListener);
     }
 
     /**
      * 绑定账户
      */
-    public static void bindAccount(Context context, String url, String LTAppID, String LTAppKey,
+    public static void bindAccount(Context context, boolean isServerTest, String LTAppID, String LTAppKey,
                                    String adID, String packageID,String tag,String token,
                                    OnLoginSuccessListener mListener) {
         Map<String, Object> map = new WeakHashMap<>();
@@ -42,7 +42,7 @@ public class GuestManager {
             map.put("platform_id", packageID);
             map.put("type", tag);
         }
-        LoginResultManager.bingAccount(context, url, LTAppID, LTAppKey, map, mListener);
+        LoginResultManager.bingAccount(context, isServerTest, LTAppID, LTAppKey, map, mListener);
     }
 
 }
